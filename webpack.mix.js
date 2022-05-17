d56546faaf27996e0ba42e6bcf962643e0af2d55
Vadim <microbb8@gmail.com>
2022-05-17 20:47:53 +0500

    Полностью интегрировал админку, поправил webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,16 @@
 const mix = require('laravel-mix');
 
 
-
 mix.styles([
-    "resources/assets/admin/css/adminlte.min.css",
-    "resources/assets/admin/plugins/fontawesome-free/css/all.min.css",
+    'resources/assets/admin/css/adminlte.min.css',
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css'
 ], 'public/assets/admin/css/admin.css');
 
 mix.scripts([
-    "resources/assets/admin/js/adminlte.min.js",
-    "resources/assets/admin/js/demo.js",
-    "resources/assets/admin/plugins/bootstrap/js/bootstrap.min.js",
-    "resources/assets/admin/plugins/jquery/jquery.min.js"
+    'resources/assets/admin/plugins/jquery/jquery.min.js',
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/assets/admin/js/adminlte.min.js',
+    'resources/assets/admin/js/demo.js',
 ], 'public/assets/admin/js/admin.js');
 
 mix.copyDirectory(
@@ -27,5 +26,10 @@ mix.copyDirectory(
 mix.copy(
     'resources/assets/admin/css/adminlte.min.css.map',
     'public/assets/admin/css/adminlte.min.css.map'
+);
+
+mix.copy(
+    'resources/assets/admin/js/adminlte.min.js.map',
+    'public/assets/admin/js/adminlte.min.js.map'
 );
 
