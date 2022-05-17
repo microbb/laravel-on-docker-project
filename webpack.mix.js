a5700a71ebf53605a46152123f046f0da3e13da9
Vadim <microbb8@gmail.com>
2022-05-17 19:25:26 +0500

    Интеграция adminlte3 половина

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+
+
+mix.styles([
+    "resources/assets/admin/css/adminlte.min.css",
+    "resources/assets/admin/plugins/fontawesome-free/css/all.min.css",
+], 'public/assets/admin/css/admin.css');
+
+mix.scripts([
+    "resources/assets/admin/js/adminlte.min.js",
+    "resources/assets/admin/js/demo.js",
+    "resources/assets/admin/plugins/bootstrap/js/bootstrap.min.js",
+    "resources/assets/admin/plugins/jquery/jquery.min.js"
+], 'public/assets/admin/js/admin.js');
+
+mix.copyDirectory(
+    'resources/assets/admin/img',
+    'public/assets/admin/img'
+);
+
+mix.copyDirectory(
+    'resources/assets/admin/plugins/fontawesome-free/webfonts',
+    'public/assets/admin/webfonts'
+);
+
+mix.copy(
+    'resources/assets/admin/css/adminlte.min.css.map',
+    'public/assets/admin/css/adminlte.min.css.map'
+);
+
